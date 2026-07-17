04f6a73f10f8343a7ea2aee86430a4a84707672e
Devindi Prasadini <devindiprasadini607@gmail.com>
2026-07-17 12:57:01 +0530

    day 28✔️

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,11 @@
 import Product from "../models/product.js"
 import isAdmin from "./userController.js"
 
-export async function createProduct(req, res){
+export async function createProduct(req, res) {
 
-    if(!isAdmin(req)){
+    if (!isAdmin(req)) {
         res.status(403).json({
-            message:"access denied.admins only"
+            message: "access denied.admins only"
         })
         return
     }
@@ -24,13 +24,13 @@ export async function createProduct(req, res){
             })
             return//!=not (user admin knk nemenm)
     }*/
-    try{//eka id eken product dekak tiyed blnn oni
+    try {//eka id eken product dekak tiyed blnn oni
         //validation
         const existingProduct = await Product.findOne({
-            productId : req.body.productId//user product ekak create krddi already e id tiye nm eyava hoyala denv
+            productId: req.body.productId//user product ekak create krddi already e id tiye nm eyava hoyala denv
         })//ehema knk natnm null vatenv
 
-        if(existingProduct!=null){
+        if (existingProduct != null) {
             res.status(400).json({
                 message: " product with this productId already exist"
             })
@@ -39,137 +39,156 @@ export async function createProduct(req, res){
 
         const newProduct = new Product({
             productId: req.body.productId,
-            name : req.body.name,
-            altNames:req.body.altNames,
-            price:req.body.price,
-            labelledPrice :req.body.labelledPrice,
-            description:req.body.description,
-            images:req.body.images,
-            brand : req.body.brand,
-            model : req.body.model,
-            category:req.body.category,
-            stock:req.body.stock
+            name: req.body.name,
+            altNames: req.body.altNames,
+            price: req.body.price,
+            labelledPrice: req.body.labelledPrice,
+            description: req.body.description,
+            images: req.body.images,
+            brand: req.body.brand,
+            model: req.body.model,
+            category: req.body.category,
+            stock: req.body.stock
         })
 
         await newProduct.save()
 
-        res.status(201).json({ message :"product created successfully"})
-        
+        res.status(201).json({ message: "product created successfully" })
 
-    }catch(error){
+
+    } catch (error) {
         res.status(500).json({
-            message : "error creating product"
+            message: "error creating product"
         })
 
     }
 }
 
-export async function getAllProducts(req, res){
+export async function getAllProducts(req, res) {
     console.log("fetching products...")
-    try{
-        if(isAdmin(req)){//admin knk visthara illanv nm
+    try {
+        if (isAdmin(req)) {//admin knk visthara illanv nm
             const products = await Product.find()
 
-        res.json(products)
+            res.json(products)
 
 
-        }else{//mormal knk illanvnm
-            const products = await Product.find({isAvailable: true});
+        } else {//mormal knk illanvnm
+            const products = await Product.find({ isAvailable: true });
             res.json(products);
         }//available true aya hoynn
-        
-    }catch(error){
+
+    } catch (error) {
         res.status(500).json({
-            message : "error fetching products"
+            message: "error fetching products"
         })
 
     }
 }
-export async function deleteProduct(req, res){
-    if(!isAdmin(req)){//product delete krnn admin knkd kiyl blanav
+export async function deleteProduct(req, res) {
+    if (!isAdmin(req)) {//product delete krnn admin knkd kiyl blanav
         res.status(403).json({
-            message : "access denied. admins only",
+            message: "access denied. admins only",
         });
         return
     }
-    try{
+    try {
         await Product.deleteOne({
             productId: req.params.productId//req eke parameter vl productId vidiht apu eka
         })
         res.json({
-            message :  "product deleted successfully"
+            message: "product deleted successfully"
         })
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            message : "error deleting product",
+            message: "error deleting product",
         })
     }
 }
 
-export async function updateProduct(req, res){
-//update venn oni kavada kiyala denv
-//postman eke localhost:3000/products/id eka
-    if(!isAdmin(req)){
+export async function updateProduct(req, res) {
+    //update venn oni kavada kiyala denv
+    //postman eke localhost:3000/products/id eka
+    if (!isAdmin(req)) {
         res.status(403).json({
-            message : "access denied. admins only",
+            message: "access denied. admins only",
         });
         return
     }
-    try{
+    try {
         await Product.updateOne({
             productId: req.params.productId
-            
 
-        },{
-            name : req.body.name,
-            altNames:req.body.altNames,
-            price : req.body.price,
-            labelledPrice :req.body.labelledPrice,
-            description:req.body.description,
-            images:req.body.images,
-            brand : req.body.brand,
-            model : req.body.model,
-            category:req.body.category,
-            stock:req.body.stock,
-            isAvailable:req.body.isAvailable
+
+        }, {
+            name: req.body.name,
+            altNames: req.body.altNames,
+            price: req.body.price,
+            labelledPrice: req.body.labelledPrice,
+            description: req.body.description,
+            images: req.body.images,
+            brand: req.body.brand,
+            model: req.body.model,
+            category: req.body.category,
+            stock: req.body.stock,
+            isAvailable: req.body.isAvailable
 
         })
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            message : "error updating product",
+            message: "error updating product",
         });
     }
 }
 export async function getProductById(req, res) {
-    try{
-       const product = await Product.findOne({// e id eken product eka tiyed blnv
-        productId: req.params.productId
-       }) 
-       if(product==null){
-        res.status(400).json({//ntnm null kiyala yvnv
-            message : "product not found"
+    try {
+        const product = await Product.findOne({// e id eken product eka tiyed blnv
+            productId: req.params.productId
         })
-       }else{
-        if(product.isAvailable){//tiye nm ona knkt blnn yavann
-            res.json(product)
-            
-        }else{
-            if(isAdmin(req)){
+        if (product == null) {
+            res.status(400).json({//ntnm null kiyala yvnv
+                message: "product not found"
+            })
+        } else {
+            if (product.isAvailable) {//tiye nm ona knkt blnn yavann
                 res.json(product)
-            }else{
-                res.status(403).json({
-                    message : "acess denied. admins only",
-                })
-            }
-        }
 
-       }
-    }catch(error){
+            } else {
+                if (isAdmin(req)) {
+                    res.json(product)
+                } else {
+                    res.status(403).json({
+                        message: "acess denied. admins only",
+                    })
+                }
+            }
+
+        }
+    } catch (error) {
         res.status(500).json({
-            message : "error fetching product",
+            message: "error fetching product",
         })
     }
-    
+
+}
+
+export async function searchProducts(req, res) {
+    try {
+        const query = req.params.query
+        const products = await Product.find({
+            $or: [
+                { name: { $regex: query, $options: "i" } },
+                { description: { $regex: query, $options: "i" } },
+                { altNames : {$elemMatch : {$regex: query, $options: "i"}}}
+            ]
+        })
+        res.json(products)
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error searching products"
+        })
+    }
 }
